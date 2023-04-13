bcb3e89b9837710c79502e7839abaa02a3a87c6e
sithomo022 <sithomo022@student.wethinkcode.co.za>
2023-04-13 10:25:54 +0200

    try to connect using IP address

--- Client.java
+++ Client.java
@@ -78,7 +78,7 @@ public class Client {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your username for the group chat: ");
         String username = scanner.nextLine();
-        Socket socket = new Socket("localhost", 8000);
+        Socket socket = new Socket("20.20.10.139", 8000);
         Client client = new Client(socket, username);
         client.listenForMessage();
         client.sendMessage();
